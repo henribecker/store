5c061c4e777f7827bb1d2a46b7517f7a71f58ff3
Henrique Becker <henribecker18@gmail.com>
2022-10-20 15:44:19 -0300

    add files de imagens

--- app/models.py
+++ app/models.py
@@ -1,3 +1,26 @@
+import PIL
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Produto(models.Model):
+    titulo = models.CharField(max_length=50, null=False, blank=False)
+    descricao = models.TextField()
+    fotos = models.ImageField(upload_to='produto/% Y/% m/ % d')
+    valor = models.DecimalField(max_digits=100, decimal_places=2, null=False, blank=True)
+    data_criacao = models.DateTimeField(auto_created=True)
+    upgrade = models.DateTimeField(auto_now_add=True)
+    criador = models.ForeignKey(User, on_delete=models.PROTECT, null=False, blank=False)
+    ativo = models.BooleanField(default=False, null=False, blank=False)
+
+    def __str__(self) -> str:
+        return self.titulo
+
+
+class Banner(models.Model):
+    titulo = models.CharField(max_length=50, null=False, blank=False)
+    fotos = models.ImageField(upload_to='banners/% Y/% m/ % d', null=False, blank=False)
+    data_criacao = models.DateTimeField(auto_created=True)
+    upgrade = models.DateTimeField(auto_now_add=True)
+    criador = models.ForeignKey(User, on_delete=models.PROTECT, null=False, blank=False)
+    ativo = models.BooleanField(default=False, null=False, blank=False)
